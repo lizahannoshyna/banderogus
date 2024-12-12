c04f665c4ce79095fa660d43f6193278ef6dca38
Liza Hannoshyna <fox8pro8@gmail.com>
2024-12-12 15:30:17 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.getElementById("formBtn").addEventListener("click", function () {
+  const emailInput = document.getElementById("email").value;
+  const nameInput = document.getElementById('name').value.trim();
+  
+    if (!nameInput && emailInput) {
+      alert("Будь ласка, заповніть поле 'Ім'я'");
+    } else if (nameInput && !emailInput) {
+      alert("Будь ласка, заповніть поле 'Email'");
+    } else if (!nameInput && !emailInput) {
+      alert("Будь ласка, заповніть обидва поля");
+    } else {
+      alert("Дякую! Гуся запущено 🚀");
+    }
+
+    if (emailInput.includes("@")) {
+      alert("Бандерогуся успішно запущено! 🚀");
+    } else {
+      alert("Неправильний email");
+    }
+});
